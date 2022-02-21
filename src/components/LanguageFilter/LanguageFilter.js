b692a41fac40ca56197ef47f77fd8c1a663312b8
YanaHorbachova <maya69877@gmail.com>
2022-02-21 10:06:07 +0200

    fix LanguageFilter

--- src/components/LanguageFilter/LanguageFilter.js
+++ src/components/LanguageFilter/LanguageFilter.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {ReactComponent as English} from '../../icon/united-kingdom.svg'
 import {ReactComponent as Arrow} from '../../icon/arrow-to-down.svg'
 import {ReactComponent as ArrowUp} from '../../icon/up-arrow.svg'
@@ -5,22 +7,32 @@ import {ReactComponent as ArrowUp} from '../../icon/up-arrow.svg'
 import s from "./LanguageFilter.module.css";
 
 const LanguageFilter = ({items, level}) => { 
+    const [checked, setChecked] = useState(false);
+
+    function onChecked() {
+        setChecked(!checked);
+      }
+
     return (
         <form className={s.listItems}>
             <div className = {s.item}>
-                <input type="checkbox" name="text" id='1-1' checked/>                        
+                <input type="checkbox" name="text" id='1-1' checked={checked} onChange={onChecked} />                        
                         <label for='1-1'> <span className={s.flag}><English/></span>Английский</label>                    
                         <p className={s.total}>603</p>
                         <ArrowUp className={s.arrow}/>
             </div>
             <div>
-                {level.map(({text,total,id}) => (
-                    <div className = {s.level}>                   
-                        <input type="checkbox" name="text" id={id}/>
-                        <label for={id}>{text}</label>                    
-                        <p className={s.total}>{total}</p>                        
-                    </div>
-                ))}
+               {checked? 
+                <div>
+                    {level.map(({text,total,id}) => ( 
+                    <div className = {s.level}>                 
+                            <input type="checkbox" name="text" id={id}/>
+                            <label for={id}>{text}</label>                    
+                            <p className={s.total}>{total}</p>  
+                                                
+                    </div> ))} </div>
+                : null}
+                
              </div>
 
             {items.map(({text,flag,total,id}) => (
